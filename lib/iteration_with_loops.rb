d46279693e89927fc5bfcee313a5e5112fe8f7a1
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-12 00:08:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,5 +6,12 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0 
     while element_index < src[row_index].count do
-      if src[row_index][element_index] = src[row_index][element_index].to_s
+      if src[row_index][element_index] != src[row_index][element_index].to_i
+        full_string << src[row_index][element_index]
+      end
+      element_index += 1 
+    end
+    row_index += 1 
+end
+full_string
 end
